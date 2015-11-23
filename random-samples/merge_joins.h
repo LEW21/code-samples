1a3f5c0f094f7d206bd24af931f172d4ce27f5b7
Janusz Lewandowski <lew21@xtreeme.org>
2015-11-23 01:18:47 +0100

    Add some random samples.

---
+++ random-samples/merge_joins.h
@@ -0,0 +1,51 @@
+// Fragment of sparqlxx query optimizer.
+// (https://github.com/LEW21/sparqlxx)
+
+// Simplifies things like Join(Join(A, Join(B, Null)), C) into Join(A, B, C).
+// Uses visitor/transformer pattern.
+
+#pragma once
+
+#include "../algebratransformer.h"
+
+namespace sparqlxx
+{
+	namespace AlgebraTransformers
+	{
+		struct MergeJoins : public AlgebraTransformer
+		{
+			using AlgebraTransformer::transform;
+
+			auto transform(Algebra::Join&& op) -> Algebra::Op override
+			{
+				auto new_ops = std::vector<Algebra::AnyOp>{};
+				std::function<void(Algebra::Join&&)> handle_join = [&new_ops, &handle_join](Algebra::Join&& op) -> void
+				{
+					for (auto& subop : op.ops)
+					{
+						if (subop->is<Algebra::Join>())
+							handle_join(std::move(subop->get<Algebra::Join>()));
+						else if (subop->is<Algebra::Null>())
+						{}
+						else
+							new_ops.emplace_back(subop);
+					}
+				};
+				handle_join(std::move(op));
+				op.ops = new_ops;
+
+				if (op.ops.size() == 0)
+					return Algebra::Null{};
+				else if (op.ops.size() == 1)
+					return *op.ops[0];
+				else
+					return op;
+			}
+		};
+
+		auto merge_joins(Algebra::Op&& op) -> Algebra::Op
+		{
+			return MergeJoins()(std::move(op));
+		}
+	}
+}
